34adf44b2a7f4dd5b2854019537c3f11c72cef28
migkjy <migkjy@naver.com>
2017-11-21 01:34:01 -0800

    browerHistory.replace

--- client/main.js
+++ client/main.js
@@ -11,15 +11,33 @@ import Login from './../imports/ui/Login';
 
 const unauthenticatedPages = ['/', '/signup'];
 const autheticatedPages = ['/links'];
+const onEnterPublicPage = () => {
+  if (Meteor.userId()) {
+    // push로는 과거로 못돌아가고 계속 루핑현상이 생길 수 있다. 따라서 과거 기록을 바꾸어주는 replace를 사용해준다.
+    // browserHistory.push('/links');
+    browserHistory.replace('/links');
+  }
+};
+const onEnterPrivatePage = () => {
+  if (!Meteor.userId()) {
+    // browserHistory.push('/');
+    browserHistory.replace('/');
+  }
+};
+
+// enter시에 변경
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/links" component={Link} />
+    <Route path="/" component={Login} onEnter={onEnterPublicPage} />
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage} />
+    <Route path="/links" component={Link} onEnter={onEnterPrivatePage} />
     <Route path="*" component={NotFound} />
   </Router>
 );
-window.browserHistory = browserHistory;
+
+// window.browserHistory = browserHistory;
+
+// 지속적으로 변경
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   const pathname = browserHistory.getCurrentLocation().pathname;
@@ -27,9 +45,11 @@ Tracker.autorun(() => {
   const isAuthenticatedPages = autheticatedPages.includes(pathname);
 
   if (isUnauthenticatedPages && isAuthenticated) {
-    browserHistory.push('/links');
+    // browserHistory.push('/links');
+    browserHistory.replace('/links');
   } else if (isAuthenticatedPages && !isAuthenticated) {
-    browserHistory.push('/');
+    // browserHistory.push('/');
+    browserHistory.replace('/');
   }
 });
 
