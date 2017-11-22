fcd1774e2b2fd2e23eb2a2343e98c710e3e6a161
migkjy <migkjy@naver.com>
2017-11-22 03:17:18 -0800

    lecture71

--- imports/api/links.js
+++ imports/api/links.js
@@ -8,5 +8,10 @@ if (Meteor.isServer) {
   //   'link'라는 param이 들어오면 뒤의 함수를 실행시킨다.
   // Meteor.publish method is only available on the server
 
-  Meteor.publish('links', () => Links.find({}));
+  Meteor.publish('links', function () {
+    // Meteor.userID is not working in Meteor.pulish
+    // so used return to get only data, not Meteor.userId
+    // 그런데 아래 this는 어쨌거나 Meteor이구만. 복잡하네
+    return Links.find({ userId: this.userId });
+  });
 }
